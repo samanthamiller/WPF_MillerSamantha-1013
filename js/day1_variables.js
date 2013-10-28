fb157266f095558d3115e8480776fedb7c8d8cb2
Samantha <miller.m.samantha@gmail.com>
2013-10-28 12:06:01 -0400

    Added notes to classExampleWPF. Used prompt commant.

--- js/day1_variables.js
+++ js/day1_variables.js
@@ -2,7 +2,7 @@
 /*  Multi line comment. */
 
 var name;
-name = "Samantha Miller"; // String example
+name = prompt("What is your name?");
 var age = 19; // Number example
 var pet = "Stella";
 var petAge = "6";
@@ -20,9 +20,9 @@ console.log(parseInt(petAge) + Number(age) + age*1);
 
 var outputString;
 outputString =
-"User age is: " + age + " "+
-"User name is: " + name + " "+
-"User pet name is: " + pet + " "+
-"User pet age is: "+ petAge;
+"User age is: " +"\t" + age + "\n"+
+"User name is: " +"\t" + name + " \n"+
+"User pet name is: " +"\t" + pet + " \n"+
+"User pet age is: " +"\t" + petAge;
 
 console.log(outputString);
