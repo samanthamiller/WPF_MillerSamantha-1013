a7d8cb7a1d5ee4ae2647a29c954a2cda8be3d25b
Samantha <miller.m.samantha@gmail.com>
2013-10-28 11:58:40 -0400

    Concatenated variables to console.log()

--- js/day1_variables.js
+++ js/day1_variables.js
@@ -4,4 +4,25 @@
 var name;
 name = "Samantha Miller"; // String example
 var age = 19; // Number example
+var pet = "Stella";
+var petAge = "6";
+console.log(age);
+age = age + 1;
 console.log(name);
+console.log(age);
+age = age - 10;
+console.log(age);
+age = age*2;
+console.log(age);
+console.log(name + age);
+
+console.log(parseInt(petAge) + Number(age) + age*1);
+
+var outputString;
+outputString =
+"User age is: " + age + " "+
+"User name is: " + name + " "+
+"User pet name is: " + pet + " "+
+"User pet age is: "+ petAge;
+
+console.log(outputString);
