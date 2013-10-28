8196be6bb26c7a3500111709637c9e37c45c85fd
Samantha <miller.m.samantha@gmail.com>
2013-10-28 12:26:35 -0400

    Created volume formula based of user input

--- js/shape.js
+++ js/shape.js
@@ -1,1 +1,7 @@
-var length =
+var length = prompt("What is the length of the rectangle?");
+var height = prompt("What is the height of the rectangle?");
+var width = prompt("What is the width of the rectangle?");
+
+var volume = Number(length)*Number(height)*Number(width);
+
+console.log("The length is " +length+ ", the height is " +height+ " and the width is " +width+ ". The volume of the rectangle is " +volume+ ".");
