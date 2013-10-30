3458edc5fa215b44388a85892f3f125d6e720e0c
Samantha <miller.m.samantha@gmail.com>
2013-10-30 14:13:45 -0400

    Relational Expression
    Started Relational expression screencast follow along withing
    ConditionalExample.js document

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -1,3 +1,4 @@
+// Following along with Conditional Example Screencast
 // Make a conditional to print out a chunk of code if child is old enough to ride a roller coaster
 
 var oldEnough = false;
@@ -8,3 +9,5 @@ if(oldEnough)
 	console.log("You can ride the coaster!");
 }
 console.log("What comes after?");
+
+// Following along with Relational Expression Screencast
