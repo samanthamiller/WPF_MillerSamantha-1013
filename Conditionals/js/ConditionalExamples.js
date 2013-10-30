df5241515557ab6484c1d1ac2e45f87e3c137b18
Samantha <miller.m.samantha@gmail.com>
2013-10-30 14:11:11 -0400

    Conditional Example
    Finished first example.

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -1,9 +1,10 @@
 // Make a conditional to print out a chunk of code if child is old enough to ride a roller coaster
 
-var oldEnough = true;
+var oldEnough = false;
 
 // If the child is old enough, print to the console "you can ride"
 if(oldEnough)
 {
 	console.log("You can ride the coaster!");
 }
+console.log("What comes after?");
