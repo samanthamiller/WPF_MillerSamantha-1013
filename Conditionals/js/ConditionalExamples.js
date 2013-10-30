ca5503cece000ef691ddbec6298f4a2ad23b6883
Samantha <miller.m.samantha@gmail.com>
2013-10-30 14:10:01 -0400

    Conditional Example
    Set up if statment to print "You are tall enough to ride the coaster"
    if the statement is true.

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -1,1 +1,9 @@
-console.log("Hello world");
+// Make a conditional to print out a chunk of code if child is old enough to ride a roller coaster
+
+var oldEnough = true;
+
+// If the child is old enough, print to the console "you can ride"
+if(oldEnough)
+{
+	console.log("You can ride the coaster!");
+}
