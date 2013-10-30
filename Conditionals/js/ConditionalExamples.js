92646b13d906eca31d3c6f9653382514c0140c8b
Samantha <miller.m.samantha@gmail.com>
2013-10-30 15:05:50 -0400

    Logical Operators
    First part of logical operators problem complete

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -36,5 +36,26 @@ else{
 	
 	
 // Following along with Logical operators screencast
+var budget = 300;
+var iPhonePrice = 199.99
+var paycheck = 200;
+// If the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget && paycheck > 300){
+	console.log("We can buy the phone!");
+}else{
+	console.log("No phone for you!");
+}
+
 	
+
+
+
+
+
+
+
+
+
+
+
 
