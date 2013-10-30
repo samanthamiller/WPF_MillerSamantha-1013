7c8d665cf44adfbad68fe493a0e5cebdf6fc3945
Samantha <miller.m.samantha@gmail.com>
2013-10-30 14:59:55 -0400

    Relational Operators
    Started relational operators screen cast, finished else if screencast.

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -12,25 +12,29 @@ console.log("What comes after?");
 
 // Following along with Relational Expression Screencast
 // If the kid is over 48 inches in height console "You can ride the coaster"
-var kidHeight = 30;
+var kidHeight = 52;
 var minHeight = 48;
-if(kidHeight > minHeight)
-{
-	console.log("You can ride the coaster!");	
-}
-
+var wParentHeight = 45;
 
 // Following along with Condition Expression Screencast
 var sneakerLift = 2;
-if(kidHeight+sneakerLift >minHeight){
+if(kidHeight >minHeight){
 	console.log("You can ride the coaster");
 }
 
 
+// Following along with else if Screencast	
+
+else if(kidHeight> wParentHeight){
+	console.log("You can ride, but only with a parent present.");
+}
+
 // Following along with if and else Screencast
 else{
-	console.log("Sorry kid, you've got some growing to do first!")
+	console.log("Sorry kid, you've got some growing to do first!");
 	}
 	
+	
+// Following along with Relational Operators
+	
 
-// Following along with else if Screencast	
