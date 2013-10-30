7707ecfd9804c149828c02d5e1b76882568a831e
Samantha <miller.m.samantha@gmail.com>
2013-10-30 14:49:35 -0400

    Condition Expression
    Finished Condition Expression Screencast.

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -18,3 +18,10 @@ if(kidHeight > minHeight)
 {
 	console.log("You can ride the coaster!");	
 }
+
+
+// Following along with Condition Expression Screencast
+var sneakerLift = 2;
+if(kidheight+sneakerLift >minHeight){
+	console.log("You can ride the coaster");
+}
