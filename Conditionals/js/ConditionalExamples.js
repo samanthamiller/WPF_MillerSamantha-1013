2ea8e35ddc366dbb9037a9db4234a446d5a8cac1
Samantha <miller.m.samantha@gmail.com>
2013-10-30 15:06:41 -0400

    Logical Operators
    Finished logical operators screencast

--- Conditionals/js/ConditionalExamples.js
+++ Conditionals/js/ConditionalExamples.js
@@ -38,9 +38,10 @@ else{
 // Following along with Logical operators screencast
 var budget = 300;
 var iPhonePrice = 199.99
-var paycheck = 200;
+var wonLotter = true;
+
 // If the price of the phone is less than our budget AND if our paycheck is over 300
-if(iPhonePrice < budget && paycheck > 300){
+if(iPhonePrice < budget || wonLottery){
 	console.log("We can buy the phone!");
 }else{
 	console.log("No phone for you!");
