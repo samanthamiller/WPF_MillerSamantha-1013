0b67f14c5bd641da348c3556282a10cfc235a8ea
Samantha <miller.m.samantha@gmail.com>
2013-10-31 14:27:44 -0400

    Conditionals_worksheet
    Started third problem, tire pressure 1

--- Conditionals_worksheet/js/main.js
+++ Conditionals_worksheet/js/main.js
@@ -35,3 +35,20 @@ if(userEnteredUsername == correctUsername && userEnteredPassword == correctPassw
 }else{
 	console.log("Sorry none of the information you entered matches our records.");
 }
+
+
+// Tire Pressure I - To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires should have the same pressure.  But the front tires and the rear tires can have different pressure than each other, so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine if the tires of a given car are up to spec. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
