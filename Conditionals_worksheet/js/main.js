05596e140d04876e49e400408da3987cd833bd5c
Samantha <miller.m.samantha@gmail.com>
2013-10-31 14:31:53 -0400

    Conditionals_worksheet
    Finished conditionals worksheet

--- Conditionals_worksheet/js/main.js
+++ Conditionals_worksheet/js/main.js
@@ -38,6 +38,14 @@ if(userEnteredUsername == correctUsername && userEnteredPassword == correctPassw
 
 
 // Tire Pressure I - To meet to maintenance standards a car’s front two tires should have the same pressure and the back two tires should have the same pressure.  But the front tires and the rear tires can have different pressure than each other, so it is not necessary for all four tires’ pressure to be the same. Create a single conditional that would determine if the tires of a given car are up to spec. 
+// Given - Pressure for each tire on the car in an array
+var tirePressure = [36,36,36,38]; // in psi
+// Result variable and result console.log
+if(tirePressure[0] == tirePressure[1] && tirePressure[2] == tirePressure[3]){
+	console.log("The tires pass spec!");
+}else{
+	console.log("Get your tires checked out!");
+}
 
 
 
